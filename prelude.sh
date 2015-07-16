9e87aaa9e1341e376ed82cc1f3568e0ba41534d5
Miëtek Bak <mietek@bak.io>
2015-07-16 19:30:50 +0100

    Rearrange code

--- prelude.sh
+++ prelude.sh
@@ -1,31 +1,24 @@
 #!/usr/bin/env bash
 
-
 set -eu
 
-
 _R=
-
 
 _ARRCTR=0
 _FRMCTR=0
 _THSFRM=( bottom )
 
 
-idris_writeStr () {
-	echo "$2"
-	eval "$1="
+idris_pushFrame () {
+	eval "_FRM${_FRMCTR}=( "\${_THSFRM[@]}" )"
+	_FRMCTR=$(( _FRMCTR + 1 ))
+	_THSFRM=( "$@" )
 }
 
 
-idris_readStr () {
-	read -r "$1"
-}
-
-
-idris_error () {
-	echo "$@" >&2
-	exit 1
+idris_popFrame () {
+	_FRMCTR=$(( _FRMCTR - 1 ))
+	eval "_THSFRM=( "\${_FRM${_FRMCTR}[@]}" )"
 }
 
 
@@ -43,14 +36,18 @@ idris_indexArray () {
 }
 
 
-idris_pushFrame () {
-	eval "_FRM${_FRMCTR}=( "\${_THSFRM[@]}" )"
-	_FRMCTR=$(( _FRMCTR + 1 ))
-	_THSFRM=( "$@" )
+idris_error () {
+	echo "$@" >&2
+	exit 1
 }
 
 
-idris_popFrame () {
-	_FRMCTR=$(( _FRMCTR - 1 ))
-	eval "_THSFRM=( "\${_FRM${_FRMCTR}[@]}" )"
+idris_writeStr () {
+	echo "$2"
+	eval "$1="
+}
+
+
+idris_readStr () {
+	read -r "$1"
 }
